2ae00646f8c3a0a1c2b0a6cdd91fc918d72ac7a0
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2025-02-14 16:08:43 +0200

    Updated users, products and products Category code

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import {config} from "dotenv"
 config()
 import cors from "cors"
 
+import usersRouter from "./Router/usersRouter.js"
+import productsCategoriesRouter from "./Router/productsCategoriesRouter.js"
+import productsRouter from "./Router/productsRouter.js"
+
 //creating app 
 const app =express()
 app.use(cors())
@@ -10,12 +14,15 @@ app.use(express.json())//to allow data to be added to the Json()file
 //to access the env config file 
 app.use(express.json())
 
+app.use("/users", usersRouter)
+app.use("/productcategories", productsCategoriesRouter)
+app.use("/products", productsRouter)
+
 //creating a port 
 const PORT = 5050
 app.listen(PORT, ()=>{
     console.log("http://localhost:"+PORT)
     console.log("I am running")
 })
-
 
 
